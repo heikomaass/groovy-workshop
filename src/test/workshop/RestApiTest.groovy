9d69d23a56748fc3598106c072a46dd92941771a
Max Trense <dev@trense.info>
2014-07-01 23:19:45 +0200

    Adds skeleton for RestApiTest

---
+++ src/test/workshop/RestApiTest.groovy
@@ -0,0 +1,15 @@
+@GrabResolver(name="lightweightest", m2Compatible='true', root='https://raw.github.com/dmitart/lightweightest/master/repository')
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.0-RC2')
+@Grab(group='org.lightweightest', module='lightweightest', version='0.1')
+/**
+ * Created by Max Trense on 01.07.2014
+ * heavily inspired from http://groovykoans.org/
+ */
+
+class RestApiTest extends GroovyTestCase {
+	
+	void test_01_ConnectingToHttpService() {
+		
+	}
+	
+}
