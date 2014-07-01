9f9f54f8ff819e073bb9e8f8ac31296feb13db03
Max Trense <dev@trense.info>
2014-07-01 23:34:31 +0200

    Adds package declaration

--- src/test/workshop/RestApiTest.groovy
+++ src/test/workshop/RestApiTest.groovy
@@ -1,6 +1,9 @@
 @GrabResolver(name="lightweightest", m2Compatible='true', root='https://raw.github.com/dmitart/lightweightest/master/repository')
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.0-RC2')
 @Grab(group='org.lightweightest', module='lightweightest', version='0.1')
+
+package test.workshop
+
 /**
  * Created by Max Trense on 01.07.2014
  * heavily inspired from http://groovykoans.org/
