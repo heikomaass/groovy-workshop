24e2b1e5ddccdd0dbe9aec9d23ffd5e6a8b54dfa
Heiko Maaß <heikomaass@gmx.de>
2014-07-05 12:54:54 +0200

    Added test for Map.any()

--- src/test/groovy/workshop/CollectionsTest.groovy
+++ src/test/groovy/workshop/CollectionsTest.groovy
@@ -65,35 +65,58 @@ class CollectionsTest extends GroovyTestCase {
         def caloriesPerStep = 0.05
 
         // Use the `inject` method to transform the steps into calories
-
         // ------------ START EDITING HERE ----------------------
-
-        burnedCalories = steps.inject(0) { sum, element -> sum += element * caloriesPerStep}
-
+        burnedCalories = steps.inject(0) { sum, element -> sum += element * caloriesPerStep }
         // ------------ STOP EDITING HERE  ----------------------
 
-
         assert burnedCalories == 2202
-
     }
-    
+
     void test_06_accessingMaps() {
-        def map = [ 
-            cast: [
-                [ name: 'Fred Flintstone', voice_actor: 'Alan Reed' ],
-                [ name: 'Barney Rubble', voice_actor: 'Mel Blanc' ]
-            ]
+        def map = [
+                cast: [
+                        [name: 'Fred Flintstone', voice_actor: 'Alan Reed'],
+                        [name: 'Barney Rubble', voice_actor: 'Mel Blanc']
+                ]
         ]
         def first_actor = ''
         def second_actor = ''
-        
+
         // ------------ START EDITING HERE ----------------------
         first_actor = map.cast[0].voice_actor
         second_actor = map.cast[1].voice_actor
         // ------------ STOP EDITING HERE  ----------------------
-        
+
         assert first_actor == 'Alan Reed'
         assert second_actor == 'Mel Blanc'
     }
-    
+
+    void test_07_checkMapUsingAny() {
+
+        def cast = [ 'Fred Flintstone' : 'Alan Reed',
+                     'Barney Rubble' : 'Mel Blanc',
+                     'Wilma Flintstone' :'Jean Vander Pyl'
+        ]
+
+        // use the `any` method to find out if the cast contains a role with `Rubble` in the name.
+        def castContainsRubble;
+
+        // use the `any` method to find out if the cast contains a role with `Dino` in the name.
+        def castContainsDino;
+
+        // ------------ START EDITING HERE ----------------------
+        castContainsRubble = cast.any { name, voice_actor ->
+            name.contains("Rubble")
+        }
+
+        castContainsDino = cast.any { name, voice_actor ->
+            name.contains("Dino")
+        }
+        // ------------ STOP EDITING HERE  ----------------------
+
+        assert castContainsRubble == true
+        assert castContainsDino == false
+    }
+
+
 }
