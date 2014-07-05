4cd2b5238a6b377f195df0e13b1edb5b5981f75d
Heiko Maaß <heikomaass@gmx.de>
2014-07-05 16:11:26 +0200

    Added test for adding a method accepting a closure

--- src/test/groovy/workshop/ClosureTest.groovy
+++ src/test/groovy/workshop/ClosureTest.groovy
@@ -5,18 +5,48 @@ package workshop
  */
 class ClosureTest extends GroovyTestCase{
 
-    void test_01_settingDelegateUsingWith() {
+    void test_01_usingWith() {
         StringBuffer stringBuffer = new StringBuffer("Hello World")
         stringBuffer.with {
             append("!")
             reverse()
             append("?")
         }
-        def expected = "!dlroW olleH?"
 
+        def expected
         // ------------ START EDITING HERE ----------------------
-        assert stringBuffer.toString() == expected;
+        expected = "!dlroW olleH?"
         // ------------ STOP EDITING HERE  ----------------------
+
+        assert stringBuffer.toString() == expected;
     }
+
+    void test_02_writeOwnMethodThatAcceptsClosure() {
+
+        // In Groovy (and Ruby) there is a method on Integer called `times`
+        // which executes the given closure n-times.
+        // Example:
+        10.times { i ->
+            println "times: ${i}"
+        }
+
+
+        // Now we want to reimplement this functionality.
+        // Tip: use `delegate` to access the Integer value.
+        Integer.metaClass.timesIWant = { closure ->
+            // ------------ START EDITING HERE ----------------------
+            for (int i = 0; i < delegate; i++) {
+                print "I want "
+                closure(i)
+            }
+            // ------------ STOP EDITING HERE  ----------------------
+        }
+
+        10.timesIWant { i ->
+            println "cookies: ${i}"
+        }
+    }
+
+
 
 }
