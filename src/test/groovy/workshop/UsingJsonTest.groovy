772bf8515626a2230e2e4ed38ac257d9604426e6
Max Trense <max.trense@namics.com>
2014-07-09 13:26:05 +0200

    Adds koan for reading and writing json data

---
+++ src/test/groovy/workshop/UsingJsonTest.groovy
@@ -0,0 +1,41 @@
+package test.workshop
+
+/**
+ * Created by Max Trense on 03.07.1014
+ */
+ 
+class UsingJsonTest extends GroovyTestCase {
+	
+	def conferenceData = [
+		conference: [
+			place: 'Pforzheim',
+			start: '21.08.2014',
+			end: '23.08.2014',
+			talks: [
+				[
+					name: 'Groovy',
+					speakers: [
+						[
+							name: 'Heiko Maaß',
+							email: 'heiko.maass@namics.com'
+						], [
+							name: 'Max Trense',
+							email: 'max.trense@namics.com'
+						]
+					]
+				]
+			]
+		]
+	]
+	
+	void test_01_generateJson() {
+		def builder = new groovy.json.JsonBuilder(conferenceData)
+		def json = builder.toString()
+		assert json.contains('Pforzheim')
+		assert json.contains('Heiko Maa\\u00df')
+		assert json.contains('Max Trense')
+	}
+	
+	
+	
+}
