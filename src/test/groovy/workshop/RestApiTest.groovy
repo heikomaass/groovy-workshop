208dbb3e12c5bd34b0b79f2c5dbd67a26315b9af
Heiko Maaß <heikomaass@gmx.de>
2014-07-12 16:48:09 +0200

    Migrate dependency to build.gradle

--- src/test/groovy/workshop/RestApiTest.groovy
+++ src/test/groovy/workshop/RestApiTest.groovy
@@ -1,9 +1,9 @@
-package test.workshop
+package workshop
 
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.2')
-import groovyx.net.http.*
-import static groovyx.net.http.ContentType.*
-import static groovyx.net.http.Method.*
+import groovyx.net.http.HTTPBuilder
+import static groovyx.net.http.Method.GET
+import static groovyx.net.http.ContentType.TEXT
+import static groovyx.net.http.ContentType.JSON
 
 /**
  * Created by Max Trense on 01.07.2014
@@ -11,27 +11,27 @@ import static groovyx.net.http.Method.*
  */
 
 class RestApiTest extends GroovyTestCase {
-	
-	void test_01_GettingData() {
-		def http = new HTTPBuilder('http://httpbin.org')
-		def responseBody = http.request(GET, TEXT) {
-	        // ------------ START EDITING HERE ----------------------
-			uri.path = '/user-agent'
-			headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
-	        // ------------ STOP EDITING HERE  ----------------------
-		}
-		assert responseBody.readLines().join('\n') == '{\n  "user-agent": "Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4"\n}'
-	}
-	
-	void test_01_GettingJson() {
-		def http = new HTTPBuilder('http://httpbin.org')
-		def json = http.request(GET, JSON) {
-	        // ------------ START EDITING HERE ----------------------
-			uri.path = '/user-agent'
-			headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
-	        // ------------ STOP EDITING HERE  ----------------------
-		}
-		assert json."user-agent" == 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
-	}
-	
+
+    void test_01_gettingData() {
+        def http = new HTTPBuilder('http://httpbin.org')
+        def responseBody = http.request(GET, TEXT) {
+            // ------------ START EDITING HERE ----------------------
+            uri.path = '/user-agent'
+            headers.'User-Agent' = 'Fancy Useragent'
+            // ------------ STOP EDITING HERE  ----------------------
+        }
+        assert responseBody.readLines().join('\n') == '{\n  "user-agent": "Fancy Useragent"\n}'
+    }
+
+    void test_02_gettingJson() {
+        def http = new HTTPBuilder('http://httpbin.org')
+        def json = http.request(GET, JSON) {
+            // ------------ START EDITING HERE ----------------------
+            uri.path = '/user-agent'
+            headers.'User-Agent' = 'Fancy Useragent'
+            // ------------ STOP EDITING HERE  ----------------------
+        }
+        assert json."user-agent" == 'Fancy Useragent'
+    }
+
 }
