7d0a1e54a2968b5179609a573955189000bf4837
Heiko Maaß <heikomaass@gmx.de>
2014-07-19 12:45:21 +0200

    moving safeNavigationOperator in OperatorTest

--- src/test/groovy/workshop/GroovyBeansTest.groovy
+++ src/test/groovy/workshop/GroovyBeansTest.groovy
@@ -21,49 +21,7 @@ class GroovyBeansTest extends GroovyTestCase {
         customer.setName("Susan")
         // ------------ STOP EDITING HERE -----------------------
         assert customer.email == "SusanDoe@namics.com"
-
     }
 
-    void test_02_safeNavigationOperator() {
-        Customer customer = new Customer()
-        shouldFail(NullPointerException.class) {
-            Date created_at = customer.cart.created_at
-            // fails, because customer.cart is null
-        }
-
-        // Groovy introduced the `Safe Navigation` operator (?.)
-        // which returns an property of an object,
-        // after an automatic null check
-        Date created_at = customer?.cart?.created_at
-        assert created_at == null
-
-        // Create a method on Customer which uses the `Safe Navigation` feature.
-        // The method should return true when the customer has created a cart today.
-        Customer.metaClass.createdCartToday = { ->
-
-            // The customer can be referenced by `delegate`.
-            // Reuse the `sameDay` method.
-            // ------------ START EDITING HERE ----------------------
-            Date now = new Date()
-
-            if (delegate?.cart?.created_at) {
-                return sameDay(now, delegate.cart.created_at)
-            }
-            false
-            // ------------ STOP EDITING HERE -----------------------
-
-        }
-        Customer hans = new Customer(name: "Hans")
-        hans.cart = new Cart(created_at: new Date())
-
-        Customer peter = new Customer(name: "Peter")
-
-        assertTrue hans.createdCartToday()
-        assertFalse peter.createdCartToday()
-    }
-
-    def sameDay(Date date1, Date date2) {
-        (date1..date2).size() == 1
-    }
 
 }
