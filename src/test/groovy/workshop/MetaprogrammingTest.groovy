769e9c87edc9d811a7b93d28ffdc56bc26b65c7f
Heiko Maass <heiko.maass@namics.com>
2014-07-21 18:43:42 +0200

    Add test for creating class at runtime

--- src/test/groovy/workshop/MetaprogrammingTest.groovy
+++ src/test/groovy/workshop/MetaprogrammingTest.groovy
@@ -19,18 +19,17 @@ class A {
 class B {
     Integer customerId
     Integer loginCount;
-
 }
 
 class MetaprogrammingTest extends GroovyTestCase {
 
-    public void test_01_methodMissing() {
+    void test_01_methodMissing() {
         A a = new A();
         def hello = a.hello()
         assert hello == "hello"
     }
 
-    public void test_02_accessingProperties() {
+    void test_02_accessingProperties() {
         def b = new B(customerId: 123, loginCount: 3);
 
         // Groovy allows to access properties statically as seen below.
@@ -50,27 +49,48 @@ class MetaprogrammingTest extends GroovyTestCase {
         assert accessViaDotOperatorWithString == accessDirectly
     }
 
-   public void test_03_addMethodToClassAtRuntime() {
-       // Methods can be added at runtime using the MetaClass interface of a Java or Groovy class.
-       B b = new B(customerId: 14, loginCount: 1)
-       b.metaClass.fancyNewMethod = { ->
-           println 'Wow, I was added at runtime'
-       }
+    void test_03_addMethodToClassAtRuntime() {
+        // Methods can be added at runtime using the MetaClass interface of a Java or Groovy class.
+        B b = new B(customerId: 14, loginCount: 1)
+        b.metaClass.fancyNewMethod = { ->
+            println 'Wow, I was added at runtime'
+        }
 
-       b.fancyNewMethod()
+        b.fancyNewMethod()
 
-       // Now it's your turn. Add the `increaseLoginCountBy` method, which increases the loginCount
-       // property by the given integer value.
-       // Hint: remeber the `delegate` variable in the closure ? You might need it.
+        // Now it's your turn. Add the `increaseLoginCountBy` method, which increases the loginCount
+        // property by the given integer value.
+        // Hint: remeber the `delegate` variable in the closure ? You might need it.
 
-       // ------------ START EDITING HERE ----------------------
-       b.metaClass.increaseLoginCountBy = { Integer i ->
-           delegate.loginCount += i
-       }
-       // ------------ STOP EDITING HERE -----------------------
-       b.increaseLoginCountBy(5)
-       assert b.loginCount == 6
+        // ------------ START EDITING HERE ----------------------
+        b.metaClass.increaseLoginCountBy = { Integer i ->
+            delegate.loginCount += i
+        }
+        // ------------ STOP EDITING HERE -----------------------
+        b.increaseLoginCountBy(5)
+        assert b.loginCount == 6
+    }
 
-   }
+    void test_04_addClassAtRuntime() {
+        // Classes can be created at runtime using the `Expando` class
+        def smartphone = new Expando(manufactor: "Apple", deviceId: 5)
+        smartphone.ring = {
+            println "ring..."
+        }
+        smartphone.ring()
+        smartphone.manufactor = "HTC"
+        assert smartphone.manufactor == "HTC"
 
+        def mac
+        // Now create a new expando object
+        // ------------ START EDITING HERE ----------------------
+        mac = new Expando(price: 2000, type: "Notebook")
+        mac.calculate = { int a, int b ->
+            a + b
+        }
+        // ------------ STOP EDITING HERE -----------------------
+        assert mac.price == 2000;
+        assert mac.type == "Notebook"
+        assert mac.calculate(1, 2) == 3
+    }
 }
