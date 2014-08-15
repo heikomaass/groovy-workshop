472c178209f646e4923db2a0358d139c9cdd644d
Max Trense <max.trense@namics.com>
2014-08-15 11:07:56 +0200

    Adds Event class with @ToString and @Delegate

--- src/test/groovy/workshop/K9AST.groovy
+++ src/test/groovy/workshop/K9AST.groovy
@@ -1,8 +1,17 @@
 package workshop
 
+import groovy.lang.Delegate
+import groovy.transform.ToString
+
 /**
  * Created by hmaass on 21.07.14.
  */
-class K9AST {
-
+class K9AST extends GroovyTestCase {
+	
+	@ToString
+	class Event {
+		@Delegate Date when
+		String title, url
+	}
+	
 }
